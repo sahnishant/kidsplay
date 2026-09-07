5f76ccf5bd17c10d82ec5ade938d95281ee879d1
Nishant Sah <134548275+sahnishant@users.noreply.github.com>
2026-09-07 13:36:47 +0530

    Route Forest Level 3 to practical grove scene

--- src/ui/ForestWorldDepthViewport.svelte
+++ src/ui/ForestWorldDepthViewport.svelte
@@ -9,12 +9,18 @@
   } = $props();
 
   let isTownMission = $derived(mission.worldActionRef?.startsWith('town.') ?? false);
+  let isBusyGrove = $derived(mission.worldActionRef === 'forest.world-depth.l3.grove-return');
 </script>
 
 {#if isTownMission}
   {#await import('./TownWorldDepthViewport-Practical.svelte') then module}
     {@const TownWorldDepthViewport = module.default}
     <TownWorldDepthViewport {mission} {childName} {onComplete} {onExit} />
+  {/await}
+{:else if isBusyGrove}
+  {#await import('./ForestGroveMissionViewport.svelte') then module}
+    {@const ForestGroveMissionViewport = module.default}
+    <ForestGroveMissionViewport {mission} {childName} {onComplete} {onExit} />
   {/await}
 {:else}
   {#await import('./ForestWorldDepthMissionViewport-Practical.svelte') then module}
